e57ff53f82a9f5f43a824989f9a96b9f8ada4a04
2yc <dame2yc@gmail.com>
2023-11-09 17:17:15 +0900

    5-21 push

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -23,7 +23,29 @@ async function loadFakeData(numUsers: number = 10){
       for(let i=0;i<numUsers;i++){
         await client.query("insert into public.users (username,password,avatar) values ($1,$2,$3)",[faker.internet.userName(),"password",faker.image.avatar()])
       }
-
+      const res = await client.query("select id from public.users order by created_at desc limit $1",[numUsers])
+      console.log(res.rows);
+      for (const row of res.rows) {
+        for (let i = 0; i < Math.ceil(Math.random() * 50); i++) {
+          await client.query(
+            "insert into public.posts (user_id, content) values ($1, $2)",
+            [row.id, faker.lorem.sentence()]
+          );
+        }
+      }
+  
+      for (const row1 of res.rows) {
+        for (const row2 of res.rows) {
+          if (row1.id != row2.id) {
+            if (Math.random() > 0.5) {
+              await client.query(
+                "insert into follows (user_id, follower_id) values ($1, $2)",
+                [row1.id, row2.id]
+              );
+            }
+          }
+        }
+      }
       await client.query("commit") // 트랜잭션에 수행된 작업을 적용
     }catch(error){
       await client.query("rollback"); //쿼리를 실행하여 트랜잭션을 롤백
